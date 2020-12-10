a3c28158d28df0111e7cf24fb82da329a180f32f
Kara P <quetzlcoatlus@gmail.com>
2020-12-10 17:21:04 +0100

    minor sizing changes

--- client/src/components/user/UserProfile.js
+++ client/src/components/user/UserProfile.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './UserProfile.css';
 
 export default class UserProfile extends Component {
   render() {
     return (
-      <div>
+      <div class="userProfile">
         its-a me!
-        <img src="https://cdn.vox-cdn.com/thumbor/6ur6WKC8RG9cOP-ZA6rbkwfeaPU=/0x0:1700x960/920x518/filters:focal(714x344:986x616):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/57514059/mario.0.jpg" alt="mario"/>
+        <p>
+          <img src="https://cdn.vox-cdn.com/thumbor/6ur6WKC8RG9cOP-ZA6rbkwfeaPU=/0x0:1700x960/920x518/filters:focal(714x344:986x616):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/57514059/mario.0.jpg" alt="mario"/>
+        </p>
       </div>
     )
   }
