710e94a29e55f11e9ceb0d15675879d30e418441
Kara P <quetzlcoatlus@gmail.com>
2020-12-10 15:38:16 +0100

    basic routing

---
+++ client/src/components/user/UserProfile.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class UserProfile extends Component {
+  render() {
+    return (
+      <div>
+        its-a me!
+        <img src="https://cdn.vox-cdn.com/thumbor/6ur6WKC8RG9cOP-ZA6rbkwfeaPU=/0x0:1700x960/920x518/filters:focal(714x344:986x616):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/57514059/mario.0.jpg" alt="mario"/>
+      </div>
+    )
+  }
+}
