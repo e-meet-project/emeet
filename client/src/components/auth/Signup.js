3af653dc2c9123eef74c7cf2db25eacda3b0ad97
Kara P <quetzlcoatlus@gmail.com>
2020-12-16 16:03:07 +0100

    styling -bootstrap&CSS

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { signup } from '../../services/auth'
 import './auth.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Form, Button, Alert, Container } from 'react-bootstrap';
 
 
 // client/src/services/auth.js <--- relative path
@@ -50,56 +52,106 @@ export default class Signup extends Component {
     console.log(this.props)
 
     return (
-      <div>
+      <div class="centerAuth">
 
-        <h1>Create your account</h1>
+        <Container>
+          <div className="boxOn">
 
-        <p className="error">
-            {this.state.message}
-        </p>
-
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <label htmlFor="username">Username </label>
-              <input
-                    type = "text"
-                    name = "username"
-                    id = "username"
-                    value = {this.state.username}
-                    onChange = {this.handleChange}
-                />
-          </p>
-
-          <p>
-            <label htmlFor="password">Password </label>
-              <input
-                    type = "password"
-                    name = "password"
-                    id = "password"
-                    value = {this.state.password}
-                    onChange = {this.handleChange}
-                />  
-            </p>
-
-            <p>
-            <label htmlFor="profileImage">Profile Picture </label>
-                <input
-                    type = "file"
-                    name = "profileImage"
-                    id = "profileImage"
-                    value = {this.state.profileImage}
-                    onChange = {this.handleChange}
-                />  
-            </p>          
-          
-          <button type='submit' >
-            Sign up 
-          </button>
-        
-        </form>
-        
-        Already have an account <Link to = "/login">Login </Link>
+            <div>
+              <h1>Create your account</h1>
+            
+              <p className="error">
+                  {this.state.message}
+              </p>
+            </div>
     
+    
+            <Form onSubmit={this.handleSubmit}>
+            <Form.Group>
+                <Form.Label  htmlFor="username">Username </Form.Label>
+                  <Form.Control 
+                        type = "text"
+                        name = "username"
+                        id = "username"
+                        value = {this.state.username}
+                        onChange = {this.handleChange}
+                    />
+              </Form.Group>
+    
+    
+              <Form.Group>
+                <Form.Group htmlFor="password">Password </Form.Group>
+                  <Form.Control 
+                        type = "password"
+                        name = "password"
+                        id = "password"
+                        value = {this.state.password}
+                        onChange = {this.handleChange}
+                    />  
+                </Form.Group>
+    
+                <Form.Group>
+{/* 
+                    <Form.File 
+                        id="custom-file"
+                        type="file"
+                        class="custom-file-input"
+                        label
+                        value = {this.state.profileImage}
+                        onChange = {this.handleChange}
+                    />  
+
+                <Form.Label  
+                  htmlFor="profileImage"
+                  for="custom-file" 
+                  class="custom-file-label"
+                  >
+                    Profile Picture 2 
+                  </Form.Label> */}
+                {/* <Form.Label  for="custom-file" class="custom-file-label">
+
+                </Form.Label> */}
+
+                  {/* <Form> */}
+                    {/* <Form.File
+                      id="custom-file"
+                      type="file"
+                      class="custom-file-input"
+                      lablel="Upload a profile picture"
+                      custom
+                    />
+                    <label for="custom-file" class="custom-file-label">
+
+                    </label> */}
+                  {/* </Form> */}
+                  <br />
+                <div className="profileImageUpload">
+                  <Form.Label  htmlFor="profileImage">Uplodad a Profile Picture? </Form.Label>
+              
+                      <div class="profileImageUploadContainer">
+                        <Form.Control 
+                            type = "file"
+                            name = "profileImage"
+                            id = "profileImage"
+                            value = {this.state.profileImage}
+                            onChange = {this.handleChange}
+                            class="profileImageUpload"
+                        />  
+                      </div>
+                </div>
+
+                </Form.Group>          
+              
+              <Button type='submit' >
+                Sign up 
+              </Button>
+            
+            </Form>
+              <div className="signupLoginSwitch">
+                Already have an account <Link to = "/login">Login </Link>
+              </div>
+          </div>
+        </Container>
       </div>
     )
   }
